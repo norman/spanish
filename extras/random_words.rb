3e9346e33050b951b2794e0f4d672edb0a69e1ed
Norman Clarke <norman@njclarke.com>
2010-07-09 14:06:25 -0300

    Accept rule names on command line.

--- extras/random_words.rb
+++ extras/random_words.rb
@@ -5,14 +5,15 @@ Bundler.setup
 require File.expand_path("../../init.rb", __FILE__)
 
 words = File.read(File.expand_path("../spanish.txt", __FILE__)).split("\n")
+rules = ARGV.map(&:to_sym)
 50.times do
   begin
     word = words[rand(words.length)]
-    trans = Spanish.get_sounds(word)
+    trans = Spanish.get_sounds(word, :seseo, :zheismo, :aspiration)
     l = 20
     puts "%#{l}s %#{l}s" % [
         word,
-        Spanish::Syllable.syllabify(word).map {|s| s.to_s }.join(" ")
+        Spanish.get_syllables_ipa(word, *rules)
     ]
   rescue => e
     puts "Died on #{word}"
--- extras/word.rb
+++ extras/word.rb
@@ -3,9 +3,4 @@ require "rubygems"
 require "bundler"
 Bundler.setup
 require File.expand_path("../../init.rb", __FILE__)
-
-until ARGV.empty?
-  word = ARGV.shift
-  trans = Spanish.get_sounds(word)
-  puts Spanish::Syllable.syllabify(word).map {|s| s.to_s }.join(" ")
-end
+puts Spanish.get_syllables_ipa(ARGV.shift, *ARGV.map(&:to_sym))
